3928568599184ad521401bf86eb3b1f1415b436a
jake-schmm <jake.schmm@gmail.com>
2023-04-20 20:49:40 -0400

    Responsive

--- show_entries.php
+++ show_entries.php
@@ -6,6 +6,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<style type="text/css">
 		.container {
 			margin-top: 50px;
